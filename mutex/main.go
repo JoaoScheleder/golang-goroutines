7ec9d1edc71a046d6fc97217e1db4008166c5c81
JoaoScheleder <jg.scheleder@hotmail.com>
2026-02-25 15:44:14 -0300

    implement concurrent income processing with goroutines and mutex for bank balance calculation

--- mutex/main.go
+++ mutex/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+type Income struct {
+	Source string
+	Amount int
+}
+
+var wg sync.WaitGroup
+
+func main() {
+	var bankBalance int = 0
+	var balance sync.Mutex
+	incomes := []Income{
+		{Source: "Job", Amount: 5000},
+		{Source: "Freelance", Amount: 2000},
+		{Source: "Investments", Amount: 1000},
+	}
+
+	wg.Add(len(incomes))
+	for i, income := range incomes {
+		go func(i int, income Income) {
+			defer wg.Done()
+			for week := 1; week <= 52; week++ {
+				balance.Lock()
+				bankBalance += income.Amount
+				fmt.Printf("Week %d: %s - $%d\n", week, income.Source, income.Amount)
+				balance.Unlock()
+			}
+		}(i, income)
+	}
+
+	wg.Wait()
+
+	fmt.Printf("Final Bank Balance: $%d\n", bankBalance)
 }
