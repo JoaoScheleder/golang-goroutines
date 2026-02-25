c0a65ae1fda548cbb28873154ec7bfa12ed9ed03
JoaoScheleder <jg.scheleder@hotmail.com>
2026-02-25 11:36:07 -0300

    refactor printSomething function to use WaitGroup for synchronization; update main function for concurrent execution

--- main.go
+++ main.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func printSomething(s string) {
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Println(s)
 }
 
 func main() {
+	var wg sync.WaitGroup
 
 	words := []string{"Hello", "World", "Go", "Programming"}
 
+	wg.Add(len(words))
+
 	for i, word := range words {
-		go printSomething(fmt.Sprintf("%d: %s", i, word))
+		go printSomething(fmt.Sprintf("%d: %s", i, word), &wg)
 	}
 
-	time.Sleep(1 * time.Second)
+	wg.Wait()
+	wg.Add(1)
+	printSomething("Something else", &wg)
 }
