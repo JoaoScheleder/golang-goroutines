f58afcb6d41e2d6df1e790916577a8dc9d7bb91e
JoaoScheleder <jg.scheleder@hotmail.com>
2026-02-25 16:09:36 -0300

    implement pizza order processing with success and failure messages; add producer close method

---
+++ producer_consumer/go.mod
@@ -0,0 +1,3 @@
+module golang-goroutines-prod-consumer
+
+go 1.25.7
--- producer_consumer/main.go
+++ producer_consumer/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 const NumberOfPizzas = 10
 
 var pizzasMade, pizzasFailed, total int
@@ -15,6 +21,108 @@ type PizzaOrder struct {
 	success     bool
 }
 
+func (p *Producer) Close() error {
+	ch := make(chan error)
+	p.quit <- ch
+	return <-ch
+}
+
+var seed = time.Now().UnixNano()
+var r = rand.New(rand.NewSource(seed))
+
+func makePizza(pizzaNumber int) *PizzaOrder {
+	pizzaNumber++
+	if pizzaNumber <= NumberOfPizzas {
+		delay := r.Intn(5) + 1
+		fmt.Printf("Receveid order #%d!\n", pizzaNumber)
+		rnd := r.Intn(12) + 1
+		msg := ""
+		success := false
+
+		if rnd < 5 {
+			pizzasFailed++
+			msg = fmt.Sprintf("Failed to make pizza #%d. Not enough ingredients.", pizzaNumber)
+		} else {
+			pizzasMade++
+		}
+		total++
+
+		fmt.Printf("Making pizza #%d will take %d seconds\n", pizzaNumber, delay)
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2 {
+			msg = fmt.Sprintf("&&& Failed to make pizza #%d. Oven is broken.", pizzaNumber)
+		} else if rnd <= 4 {
+			msg = fmt.Sprintf("&&& Failed to make pizza #%d. Not enough staff.", pizzaNumber)
+		} else {
+			success = true
+			msg = fmt.Sprintf("Pizza #%d is ready!", pizzaNumber)
+		}
+
+		p := PizzaOrder{
+			pizzaNumber: pizzaNumber,
+			message:     msg,
+			success:     success,
+		}
+
+		return &p
+	}
+
+	return &PizzaOrder{
+		pizzaNumber: pizzaNumber,
+	}
+
+}
+
+func pizzeria(pizzaMaker *Producer) {
+	var i = 0
+
+	for {
+		currentPizza := makePizza(i)
+		if currentPizza != nil {
+			i = currentPizza.pizzaNumber
+			select {
+			case pizzaMaker.data <- *currentPizza:
+
+			case quitChan := <-pizzaMaker.quit:
+				close(pizzaMaker.data)
+				close(quitChan)
+				return
+			}
+		}
+	}
+}
+
 func main() {
+
+	fmt.Println("The Pizzeria is open to business!")
+	fmt.Println("----------------------------")
+
+	pizzaJob := &Producer{
+		data: make(chan PizzaOrder),
+		quit: make(chan chan error),
+	}
+
+	go pizzeria(pizzaJob)
+
+	for i := range pizzaJob.data {
+		if i.pizzaNumber <= NumberOfPizzas {
+			if i.success {
+				fmt.Printf("Order #%d: %s out for delivery!\n", i.pizzaNumber, i.message)
+			} else {
+				fmt.Printf("Order #%d: %s\n failed", i.pizzaNumber, i.message)
+			}
+		} else {
+			fmt.Println("Done making pizzas")
+			err := pizzaJob.Close()
+			if err != nil {
+				fmt.Printf("Error closing producer: %v\n", err)
+			}
+			fmt.Printf("Total pizzas made: %d\n", pizzasMade)
+			fmt.Printf("Total pizzas failed: %d\n", pizzasFailed)
+			fmt.Printf("Total orders processed: %d\n", total)
+			break
+		}
+	}
 
 }
