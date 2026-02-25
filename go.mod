8910209d8ad577a67c508a3218e8cf6f433ae8ef
JoaoScheleder <jg.scheleder@hotmail.com>
2026-02-25 11:33:20 -0300

    implement main function with goroutines for concurrent printing; add go.mod for module management

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-goroutines-course
+
+go 1.25.7
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func printSomething(s string) {
+	fmt.Println(s)
+}
+
 func main() {
 
+	words := []string{"Hello", "World", "Go", "Programming"}
+
+	for i, word := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, word))
+	}
+
+	time.Sleep(1 * time.Second)
 }
