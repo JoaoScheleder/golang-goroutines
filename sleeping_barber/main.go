1fad26e223898f5b30156023eda9e54a3b76e753
JoaoScheleder <jg.scheleder@hotmail.com>
2026-03-02 15:25:11 -0300

    refactor: replace fmt.Printf with color logging for barber messages

--- sleeping_barber/main.go
+++ sleeping_barber/main.go
@@ -4,14 +4,20 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+
+	"github.com/fatih/color"
 )
 
 var seatingCapacity = 10
-var arrivalRate = 1000
+var arrivalRate = 100
 var cutDuration = 1000 * time.Millisecond
 var timeOpen = 8 * time.Second
 
 var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+var successLog = color.New(color.FgGreen).PrintfFunc()
+var warningLog = color.New(color.FgYellow).PrintfFunc()
+var errorLog = color.New(color.FgRed).PrintfFunc()
 
 type BarberShop struct {
 	ShopCapacity    int
@@ -28,18 +34,18 @@ func (b *BarberShop) addBarber(barber string) {
 
 	go func() {
 		isSleeping := false
-		fmt.Printf("Barber %d (%s) is ready to cut hair and checking for clients\n", barberID, barber)
+		successLog("Barber %d (%s) is ready to cut hair and checking for clients\n", barberID, barber)
 		for {
 			// if there are no clients, the barber goes to sleep
 			if len(b.ClientsChan) == 0 {
-				fmt.Printf("Barber %d (%s) is sleeping\n", barberID, barber)
+				warningLog("Barber %d (%s) is sleeping\n", barberID, barber)
 				isSleeping = true
 			}
 
 			client, shopOpen := <-b.ClientsChan
 			if shopOpen {
 				if isSleeping {
-					fmt.Printf("Barber %d (%s) is waking up to cut %s's hair\n", barberID, barber, client)
+					warningLog("Barber %d (%s) is waking up to cut %s's hair\n", barberID, barber, client)
 					isSleeping = false
 				}
 				b.cutHair(barber, client)
@@ -52,38 +58,38 @@ func (b *BarberShop) addBarber(barber string) {
 }
 
 func (b *BarberShop) cutHair(barber, client string) {
-	fmt.Printf("Barber %s is cutting hair of client %s\n", barber, client)
+	successLog("Barber %s is cutting hair of client %s\n", barber, client)
 	time.Sleep(b.HairCutDuration)
-	fmt.Printf("Barber %s is done cutting hair of client %s\n", barber, client)
+	successLog("Barber %s is done cutting hair of client %s\n", barber, client)
 }
 
 func (b *BarberShop) sendBarberHome(barberID int, barber string) {
-	fmt.Printf("Barber %d (%s) is going home\n", barberID, barber)
+	warningLog("Barber %d (%s) is going home\n", barberID, barber)
 	b.BarbersDoneChan <- true
 }
 
 func (b *BarberShop) closeShopForTheDay() {
-	fmt.Println("Shop is closing for the day")
+	warningLog("Shop is closing for the day\n")
 	b.Open = false
 	close(b.ClientsChan)
 	for i := 0; i < b.NumberOfBarbers; i++ {
 		<-b.BarbersDoneChan
 	}
-	fmt.Println("---------------------------------------")
-	fmt.Println("Shop is closed for the day")
+	warningLog("---------------------------------------\n")
+	warningLog("Shop is closed for the day\n")
 }
 
 func (b *BarberShop) addClient(client string) {
-	fmt.Printf("Client arrived: %s\n", client)
+	successLog("Client arrived: %s\n", client)
 	if b.Open {
 		select {
 		case b.ClientsChan <- client:
-			fmt.Printf("%s takes a seat in the waiting room\n", client)
+			successLog("%s takes a seat in the waiting room\n", client)
 		default:
-			fmt.Printf("Shop is full, %s is leaving\n", client)
+			errorLog("Shop is full, %s is leaving\n", client)
 		}
 	} else {
-		fmt.Printf("Shop is closed, %s is leaving\n", client)
+		errorLog("Shop is closed, %s is leaving\n", client)
 	}
 }
 
@@ -102,7 +108,7 @@ func main() {
 		Open:            true,
 	}
 
-	fmt.Println("Shop is open")
+	successLog("Shop is open\n")
 
 	// add barbers
 	shop.addBarber("Frank")
